c7f000cdb9d0db4377651af34154fc4bb3227eaf
Hanif Patel <hanif2403@gmail.com>
2021-11-06 19:55:08 +0300

    fixed concat errors

--- group3proj1.py
+++ group3proj1.py
@@ -4,11 +4,11 @@ def student_status(class_avg, student_score):
         if student_score >= class_avg:
             print(student_name + ' passed with fying colours.')
         elif student_score >= class_avg-5:
-            print(student_name +' passed but almost did not make it, they got close to the class average of ' + class_avg)
+            print(student_name +' passed but almost did not make it, they got close to the class average')
         elif student_score < class_avg:
-            print(student_name + ' beat pass mark of' + pass_mark + ' but failed to beat class average of ' + class_avg)
+            print(student_name + ' beat pass mark of but failed to beat class average')
     elif student_score < pass_mark and student_score >= class_avg:
-        print(student_name + ' beat class avg of ' + class_avg +' but not pass mark of ' + pass_mark)
+        print(student_name + ' beat class avg but not pass mark' )
     else:
         print(student_name + ' failed')
 
