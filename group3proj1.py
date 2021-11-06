b85f654c361e4b62333aa87bd46bace8e84a970b
Hanif Patel <hanif2403@gmail.com>
2021-11-06 20:00:33 +0300

    edited file on previous push by zion

--- group3proj1.py
+++ group3proj1.py
@@ -8,7 +8,7 @@ def student_status(class_avg, student_score):
         elif student_score < class_avg:
             print(student_name + ' beat pass mark of but failed to beat class average')
     elif student_score < pass_mark and student_score >= class_avg:
-        print(student_name + ' beat class avg but not pass mark' )
+        print(student_name + ' beat class average but not pass mark' )
     else:
         print(student_name + ' failed')
 
